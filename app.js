44eb0e966efe65b900092c7edaa48cd087d03bb5
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-03-29 15:50:46 +0430

    01

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ async function getData(){
 }
 async function getMap(longitude,latitude){
     let width = window.screen.width > 1280 ? 1280: window.screen.width;
-    console.log(`window screen width is ${window.screen.width}
-    width is ${width}
-    `)
-    const data = await fetch(`https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${longitude},${latitude},10/1280x${width}?access_token=pk.eyJ1IjoiYW1pcjAweCIsImEiOiJjazhiYWc4MHgwOXlzM2RvM2QzaGowanczIn0.5V1QbKuJ02Gtr7AP6YmX-A`);
+    let height = window.screen.width > 1280 ? 1280: window.screen.height/2;
+    const data = await fetch(`https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${longitude},${latitude},10/${height}x${width}?access_token=pk.eyJ1IjoiYW1pcjAweCIsImEiOiJjazhiYWc4MHgwOXlzM2RvM2QzaGowanczIn0.5V1QbKuJ02Gtr7AP6YmX-A`);
     const jData = await data;
-    console.log(jData);
     return data;
 }
 const setupUi = (Data)=>{
@@ -30,8 +27,5 @@ const setupUi = (Data)=>{
         .catch(Err => console.log(Err));
 }
 getData()
-    .then(Data => {
-        console.log(Data);
-        setupUi(Data);
-    })
+    .then(Data =>setupUi(Data))
     .catch(Err => console.log(Err));
