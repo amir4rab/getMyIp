b7615c34f9134f0b58989974808eb9f6d21eb782
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-03-29 16:16:07 +0430

    02

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ async function getData(){
     return jData;
 }
 async function getMap(longitude,latitude){
-    let width = window.screen.width > 1280 ? 1280: window.screen.width;
-    let height = window.screen.width > 1280 ? 1280: window.screen.height/2;
+    let width = window.screen.width > 1280 ? 1280: Math.round(window.screen.width);
+    let height = window.screen.width > 1280 ? 1280: Math.round(window.screen.height/2);
     const data = await fetch(`https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${longitude},${latitude},10/${height}x${width}?access_token=pk.eyJ1IjoiYW1pcjAweCIsImEiOiJjazhiYWc4MHgwOXlzM2RvM2QzaGowanczIn0.5V1QbKuJ02Gtr7AP6YmX-A`);
     const jData = await data;
     return data;
